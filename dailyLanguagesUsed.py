d5b8bacba330c34ed87500cde5a69065e8117f33
Shubham Singh <shubham.singh12@infosys.com>
2019-04-09 17:18:43 +0530

    Writing Data to Excel

--- dailyLanguagesUsed.py
+++ dailyLanguagesUsed.py
@@ -2,12 +2,16 @@
 import requests
 import datetime
 import csv
+import os
+import time
 from bs4 import BeautifulSoup
 
 # global variables
 programming_languages_used = {}
 counter = 1
 ideoneString = "https://ideone.com"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file_name =  dir_path + "/" +"programming_language_used.csv"
 
 def get_content(path):
     r = requests.get(path)
@@ -17,6 +21,21 @@ def get_content(path):
     return soup
 
 def count_programming_languages_used(soup):
+    line_count = 0
+    if os.path.isfile(file_name):   
+        print("File exists") 
+        with open(file_name,'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',') 
+            for row in csv_reader:
+                print("test row ->",row[1])
+                if line_count == 0:
+                    pass
+                else:
+                    programming_languages_used[row[1]] = int(row[2])
+                line_count += 1
+        print("After reading file",programming_languages_used)
+
+
     languagesResult = soup.find_all(class_ = "header")
     for elem in languagesResult:
         spanElement = elem.find('span')
@@ -28,21 +47,41 @@ def count_programming_languages_used(soup):
             programming_languages_used[language] += 1
 
 
+def write_data_in_csvfile(programming_language_dictionary):
+    date_variable = datetime.date.today()
+
+    programming_languages = list(programming_language_dictionary.keys())
+    programming_languages_count = list(programming_language_dictionary.values())
+    print(date_variable.strftime("%d-%b-%Y"))
+
+    if os.path.isfile(file_name):
+        with open(file_name,'w+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(['Date','Programming Language','Count'])
+    
+    for index in range(0,len(programming_languages)):
+        write_in_excel = []
+        write_in_excel.append(date_variable)
+        write_in_excel.append(programming_languages[index])
+        write_in_excel.append(programming_languages_count[index])
+
+        with open(file_name,'a+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(write_in_excel)
+
+
 if __name__ == "__main__":
+    t1 = time.time()
+    if not os.path.isfile(file_name):
+        with open(file_name,'a') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(['Date','Programming Language','Count'])
 
     contents = get_content(ideoneString + '/recent/'+str(counter))    
     count_programming_languages_used(contents)
-    date_variable = datetime.date.today()
-    print(date_variable.strftime("%d-%b-%Y"))
+    write_data_in_csvfile(programming_languages_used)
     print("Languages used ->",programming_languages_used)
+    t2 = time.time()
+    print("Task done in : ",str(datetime.timedelta(seconds=t2 - t1)))
 
-    with open('programs_used_by_ideone.csv','a') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerow(['date'])
-    csvFile.close()
 
-with open('person.csv', 'w') as csvFile2:
-    writers = csv.writer(csvFile2)
-    writers.writerow(['date'])
-
-csvFile2.close()
--- part1.py
+++ part1.py
@@ -4,10 +4,12 @@ import csv
 
 # global variables
 counter = 1
-test_data = ['test']
+test_data = [4]
+test_data2 = [5,15]
 result_set = set()
 ideoneString = "https://ideone.com"
 languageList = []
+
 
 def get_content(path):
     r = requests.get(path)
@@ -15,6 +17,7 @@ def get_content(path):
     print(r.url)
     soup = BeautifulSoup(content,'html.parser')
     return soup
+
 
 def populate_languages_used(soup):
     languagesResult = soup.find_all(class_ = "header")
@@ -27,13 +30,13 @@ def get_links_from_content(soup):
     allResults = soup.find_all("strong")
     for index,result in enumerate(allResults):
         link = result.find("a")
-        print(link,languageList[index])    
-        if (languageList[index] == "C++" or languageList[index] == "C++14"):
+        if (languageList[index] == "C++" or languageList[index] == "C++14" or languageList[index] == "Java"):
             result_set.add(ideoneString+link.get('href'))
     print(result_set) 
     print("Length of result :",len(result_set))
 
-def check_standard_output(test_data_sent):
+
+def check_standard_output(test_data_sent,test_data2):
     for item in result_set:
         r = requests.get(item)
         content = r.content
@@ -46,25 +49,29 @@ def check_standard_output(test_data_sent):
         #to check the output manually
         # print(checkList) 
         test_data_sent = [str(item) for item in test_data_sent]
+        test_data2 = [str(item) for item in test_data2]
         setA = set(test_data)
+        setC = set(test_data2)
         setB = set(checkList)
-        print("Matches ->",r.url,"->",setA.issubset(setB))
+        if (setA.issubset(setB) or setC.issubset(setB)):
+            print("Matches ->",r.url,"->",setA.issubset(setB))
+            print("matches 2 ->",r.url,"->",setC.issubset(setB))
+
 
 if __name__ == "__main__":
+    # # initialize csv file
+    # with open('programs_used_by_ideone.csv','a') as csvFile:
+    #     writer = csv.writer(csvFile)
+    #     writer.writrerows(['date','programs'])
+    #     writer.close()
 
-    row = ['2', ' Marie', ' California']
-    # initialize csv file
-    with open('people.csv', 'w') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(row)
-    
-    writer.close()
-
-
-    contents = get_content(ideoneString + '/recent/'+str(counter))
-    populate_languages_used(contents)
-    get_links_from_content(contents)
-    check_standard_output(test_data)
+    while True:
+        contents = get_content(ideoneString + '/recent/'+str(counter))
+        populate_languages_used(contents)
+        get_links_from_content(contents)
+        check_standard_output(test_data,test_data2)
+        result_set.clear()
+        counter += 1
 
 
 
